325f89abd545cdda06c0b27a5323b86900c47866
Komal Thareja <kthare10@renci.org>
2025-10-06 18:32:07 -0400

    pass cluster name as query param

--- fabric_ceph/openapi_server/controllers/cluster_user_controller.py
+++ fabric_ceph/openapi_server/controllers/cluster_user_controller.py
@@ -1,20 +1,3 @@
-import connexion
-from typing import Dict
-from typing import Tuple
-from typing import Union
-
-from fabric_ceph.openapi_server.models.apply_user_response import ApplyUserResponse  # noqa: E501
-from fabric_ceph.openapi_server.models.create_user_templated_request import CreateUserTemplatedRequest  # noqa: E501
-from fabric_ceph.openapi_server.models.export_users_request import ExportUsersRequest  # noqa: E501
-from fabric_ceph.openapi_server.models.export_users_response import ExportUsersResponse  # noqa: E501
-from fabric_ceph.openapi_server.models.status200_ok_no_content import Status200OkNoContent  # noqa: E501
-from fabric_ceph.openapi_server.models.status400_bad_request import Status400BadRequest  # noqa: E501
-from fabric_ceph.openapi_server.models.status401_unauthorized import Status401Unauthorized  # noqa: E501
-from fabric_ceph.openapi_server.models.status403_forbidden import Status403Forbidden  # noqa: E501
-from fabric_ceph.openapi_server.models.status404_not_found import Status404NotFound  # noqa: E501
-from fabric_ceph.openapi_server.models.status500_internal_server_error import Status500InternalServerError  # noqa: E501
-from fabric_ceph.openapi_server.models.users import Users  # noqa: E501
-from fabric_ceph.openapi_server import util
 from fabric_ceph.response import cluster_user_controller as rc
 
 def apply_user_templated(cluster, body):  # noqa: E501
@@ -29,10 +12,7 @@ def apply_user_templated(cluster, body):  # noqa: E501
 
     :rtype: Union[ApplyUserResponse, Tuple[ApplyUserResponse, int], Tuple[ApplyUserResponse, int, Dict[str, str]]
     """
-    create_user_templated_request = body
-    if connexion.request.is_json:
-        create_user_templated_request = CreateUserTemplatedRequest.from_dict(connexion.request.get_json())  # noqa: E501
-    return 'do some magic!'
+    return rc.apply_user_templated(cluster, body)
 
 
 def delete_user(cluster, entity):  # noqa: E501
@@ -47,7 +27,7 @@ def delete_user(cluster, entity):  # noqa: E501
 
     :rtype: Union[Status200OkNoContent, Tuple[Status200OkNoContent, int], Tuple[Status200OkNoContent, int, Dict[str, str]]
     """
-    return 'do some magic!'
+    return rc.delete_user(cluster, entity)
 
 
 def export_users(cluster, body):  # noqa: E501
@@ -62,10 +42,7 @@ def export_users(cluster, body):  # noqa: E501
 
     :rtype: Union[ExportUsersResponse, Tuple[ExportUsersResponse, int], Tuple[ExportUsersResponse, int, Dict[str, str]]
     """
-    export_users_request = body
-    if connexion.request.is_json:
-        export_users_request = ExportUsersRequest.from_dict(connexion.request.get_json())  # noqa: E501
-    return 'do some magic!'
+    return rc.export_users(cluster, body)
 
 
 def list_users(cluster):  # noqa: E501
@@ -78,4 +55,4 @@ def list_users(cluster):  # noqa: E501
 
     :rtype: Union[Users, Tuple[Users, int], Tuple[Users, int, Dict[str, str]]
     """
-    return 'do some magic!'
+    return rc.list_users(cluster)

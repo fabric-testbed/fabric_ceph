ddffcb5fe5d0d7c8346b7ca5bd431250751edb3e
Komal Thareja <kthare10@renci.org>
2025-10-06 22:17:29 -0400

    permission check for operator

--- fabric_ceph/response/cluster_user_controller.py
+++ fabric_ceph/response/cluster_user_controller.py
@@ -163,7 +163,7 @@ def export_users(cluster, body):  # noqa: E501
 
     try:
         fabric_token, is_operator, bastion_login = authorize()
-        if len(export_users_request.entities) == 1 and bastion_login.lower() not in export_users_request.entities[0].lower():
+        if not is_operator and len(export_users_request.entities) == 1 and bastion_login.lower() not in export_users_request.entities[0].lower():
             return cors_401(details=f"{fabric_token.uuid}/{fabric_token.email} is not authorized!")
 
         if len(export_users_request.entities) > 1 and not is_operator:
